d0fecc67d490391603db70e51332cdcbe5cd0af9
Koopmans <koopmans.michaela@epa.gov>
2018-01-05 08:54:01 -0500

    Update fmcslookup and handlesmistring functions

--- R/fmcslookupfctn.R
+++ R/fmcslookupfctn.R
@@ -10,7 +10,7 @@ fmcslookupfctn <- function(sampleSDF) {
   MCS = batchfull[1] #Loads comparison library
   Tanimoto = 0
   Index = 0 #Sets up variables Tanimoto, and Index
-  samplesmi = ChemmineR::sdf2smiles(sampleSDF)
+  samplesmi = ChemmineR::sdf2smiles(sampleSDF) #Converts sample fragment from SDF to SMILES to be used in the output dataframe
   for(i in 1:300)
   {
 
@@ -27,24 +27,23 @@ fmcslookupfctn <- function(sampleSDF) {
   }
 
   if (Tanimoto > 0.7){
-    smiles <- ChemmineR::sdf2smiles(MCS[1]) #Stores values for best match in SMILES format
-    ChemmineR::write.SMI(smiles, file = "smiles.smi") #Creates output SMILES file
-    sigma <- dbGetQuery(lookupdb2, 'SELECT "sigma" FROM testlookup2 WHERE "ID" == :z', params = list(z = Index)) #Searches for sigma values of substructure match
+    smiles <- ChemmineR::sdf2smiles(MCS[1]) #Stores the best match molecule in SMILES format
+    ChemmineR::write.SMI(smiles, file = "smiles.smi") #Creates output SMILES file for the match molecule
+    sigma <- dbGetQuery(lookupdb2, 'SELECT "sigma" FROM testlookup2 WHERE "ID" == :z', params = list(z = Index)) #Retrieves sigma values of match molecule
     sigma.meta <- dbGetQuery(lookupdb2, 'SELECT "sigma.meta" FROM testlookup2 WHERE "ID" == :z', params = list(z = Index))
     sigma.para <- dbGetQuery(lookupdb2, 'SELECT "sigma.para" FROM testlookup2 WHERE "ID" == :z', params = list(z = Index))
     char <- c(as.character(samplesmi), as.character(smiles), as.character(Tanimoto), as.character(sigma), as.character(sigma.meta), as.character(sigma.para)) #Creates list of output values
-    outdf <- data.frame(matrix(unlist(char), nrow=1, byrow=T)) #Converts list into usable output format
-    colnames(outdf) = c("Original Fragment SMILES","MCSS Match SMILES", "Tanimoto Index", "Sigma Value", "Sigma Meta Value", "Sigma Para Value")
-    print(outdf) #Shows output values
+    outdf <- data.frame(matrix(unlist(char), nrow=1, byrow=T)) #Converts list into usable output format as a dataframe
+    colnames(outdf) = c("Original Fragment SMILES","MCSS Match SMILES", "Tanimoto Index", "Sigma Value", "Sigma Meta Value", "Sigma Para Value") #Creates column labels for the dataframe
+    print(outdf) #Shows output values in console
     #print(paste("MCSS Match SMILES: ", smiles))
     #print(paste("Tanimoto Index: ", Tanimoto))
     #print(paste("Sigma Value: ", sigma))
     #print(paste("Sigma Meta Value: ", sigma.meta))
     #print(paste("Sigma Para Value: ", sigma.para)) #Prints relevant outputs
-    #ChemmineR::plot(MCS[1], regenCoords = TRUE, print = FALSE) #Creates a visualization of output structure
-    outmcs <- fmcsR::fmcs(MCS, sampleSDF[1])
-    fmcsR::plotMCS(outmcs)
-    jsonlite::write_json(outdf, "output-json.json", dataframe = "columns")
+    outmcs <- fmcsR::fmcs(MCS, sampleSDF[1]) #Runs MCS between match molecule and original fragment to get the output information in MCS format for use in the plotMCS visualization function
+    fmcsR::plotMCS(outmcs) #Visualizes the original fragment and match molecules and highlights the similar substructure
+    jsonlite::write_json(outdf, "output-json.json", dataframe = "columns") #Writes a JSON containing the output values dataframe for use in CTS
     }
   else {
     print("No similar matches were found.")
--- R/handlesmistringfctn.R
+++ R/handlesmistringfctn.R
@@ -9,6 +9,6 @@
 handleSMIstring <- function(string) {
   samplesmiles <- string #Stores SMILES string input
   sampleSDF <- ChemmineR::smiles2sdf(samplesmiles) #Converts user-input SMILES sample string to SDF format
-  print(paste("Original Fragment SMILES: ", samplesmiles))
-  fmcslookupfctn(sampleSDF)
+  print(paste("Original Fragment SMILES: ", samplesmiles)) #Prints to console the SMILES string for the original sample fragment
+  fmcslookupfctn(sampleSDF) #Performs MCS matching on sample fragment
 }
